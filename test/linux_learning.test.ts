13c096faabc98897da22bd80bb19fd027db6d880
kohei-iwamoto-wa <bananadesu9@gmail.com>
2026-07-21 21:14:19 +0900

    スナップショットテスト

--- test/linux_learning.test.ts
+++ test/linux_learning.test.ts
@@ -1,17 +1,11 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as LinuxLearning from '../lib/linux_learning-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as LinuxLearning from '../lib/linux_learning-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/linux_learning-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new LinuxLearning.LinuxLearningStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test('Snapshot test', () => {
+    const app = new cdk.App();
+    const stack = new LinuxLearning.LinuxLearningStack(app, 'MyTestStack');
+    const template = Template.fromStack(stack);
+    expect(template.toJSON()).toMatchSnapshot();
 });
